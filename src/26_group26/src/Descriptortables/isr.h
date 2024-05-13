57ee3374dd9ac399b88f1e81e5c49dc4b8731667
Aleksander Marø Vågen <aleksandev@uia.no>
2024-05-13 12:56:37 +0000

    udated by aleks

--- src/26_group26/src/Descriptortables/isr.h
+++ src/26_group26/src/Descriptortables/isr.h
@@ -1,6 +1,23 @@
 #ifndef ISR_H
 #define ISR_H
 
+#define IRQ0 32
+#define IRQ1 33
+#define IRQ2 34
+#define IRQ3 35
+#define IRQ4 36
+#define IRQ5 37
+#define IRQ6 38
+#define IRQ7 39
+#define IRQ8 40
+#define IRQ9 41
+#define IRQ10 42
+#define IRQ11 43
+#define IRQ12 44
+#define IRQ13 45
+#define IRQ14 46
+#define IRQ15 47
+
 #include "libc/stdint.h"
 
 // Structure representing the registers saved on the stack during an interrupt.
@@ -12,5 +29,12 @@ typedef struct {
 } registers_t;
 
 void isr_handler(registers_t regs);
- 
+
+typedef void (*isr_t)(registers_t);
+void register_interrupt_handler(u8int n, isr_t handler);
+
+
+
+
+
 #endif // ISR_H
